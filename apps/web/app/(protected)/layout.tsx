4a32896ea9b0373e20b897494c9d12196bfa1809
Harsh-Vdn25 <hars.vdn16@gmail.com>
2026-01-15 22:13:52 +0530

    layout added to navigate to profile page

--- apps/web/app/(protected)/layout.tsx
+++ apps/web/app/(protected)/layout.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "@repo/ui/button";
 
-export default function ProtectedLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function ProtectedLayout({ children }: { children: ReactNode }) {
   const { token, loading } = useAuth();
   const router = useRouter();
-
+  const pathname = usePathname();
+  const playPage = pathname === "/play"; //this will throw a true or false and based on this you can navigate window is often undefined as browser API takes time
   useEffect(() => {
     if (!loading && !token) {
       router.replace("/signin");
@@ -26,5 +24,21 @@ export default function ProtectedLayout({
     );
   }
 
-  return <>{children}</>;
+  return (
+    <div className="w-screen h-screen flex flex-col bg-gray-900">
+      <header className="h-14 bg-gray-800 flex items-center justify-between px-6">
+        <h1 className="text-white font-semibold text-lg">Chess Game</h1>
+
+        <Button
+          onClick={() => {
+            router.push(playPage ? "/profile" : "/play")
+          }}
+        >
+          {playPage ? "Profile" : "Back"}
+        </Button>
+      </header>
+
+      <main className="flex-1">{children}</main>
+    </div>
+  );
 }
